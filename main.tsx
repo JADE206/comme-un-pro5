ed857e544e858c4e5979d87732a73ecbee32f1fe
JADE206 <jade.prudent1201@gmail.com>
2025-04-02 13:09:39 +0200

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import SellerDashboard from './SellerDashboard';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SellerDashboard />
+  </React.StrictMode>
+);
